265687ecc4410b6248ca283b51384bddf67d0344
xakimjon <xakimjonnomozov8@gmail.com>
2024-08-02 00:34:35 +0500

    almost done

--- courses/urls.py
+++ courses/urls.py
@@ -13,6 +13,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', auth_views.RegisterView.as_view(), name='register'),
-
     path('activate/<uidb64>/<token>/', auth_views.activate, name='activate'),
 ]
